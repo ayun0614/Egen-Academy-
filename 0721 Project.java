d7c9a73ba854fd3aa9d90d8ed70eebd09f4f945f
Kim soyoung <conpaku82@gmail.com>
2023-07-21 19:22:00 +0900

    07-21 실습
    이번주 학습한 내용을 바탕으로 실습을 진행하였다.

---
+++ 0721 Project.java
@@ -0,0 +1,118 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        // //1
+        // System.out.printf("%.2f", 5/3.0);
+        
+        // //2
+        // Scanner scan = new Scanner(System.in);
+        // double height = scan.nextDouble();
+
+        // if(height<160){
+        //     System.out.println("small");
+        // }else if(height<170){
+        //     System.out.println("medium");
+        // }else if(height<180){
+        //     System.out.println("large");
+        // }
+
+        // //3
+        // int n1 = 50, n2 = 100;
+        // System.out.println((n1>n2)?n1:n2);
+
+        // //4
+        // Scanner scan = new Scanner(System.in);
+
+        // int age;
+        // double height;
+        // char size;
+
+        // System.out.print("나이 키 입력 : ");
+
+        // age = scan.nextInt();
+        // height = scan.nextDouble();
+
+        // if(age<20){
+        //     if(height<160){
+        //         size = 'S';
+        //     }else if(height<170){
+        //         size = 'M';
+        //     }else{
+        //         size = 'L';
+        //     }
+        // }else{
+        //     if(height<165){
+        //         size = 'S';
+        //     }else if(height<175){
+        //         size = 'M';
+        //     }else{
+        //         size = 'L';
+        //     }
+        // }
+
+        // System.out.println(size);
+
+        // //5
+        // for(int i=1; i <= 50; i++){
+        //     System.out.println(i);
+        // }
+        
+        // //5-1
+        // for(int i=1; i <= 50; i++){
+        //     if(i%6==0){
+        //         System.out.println(i);
+        //     }
+        // }
+
+        // //5-2
+        // int sum = 0;
+        // for(int i=1; i <= 50; i++){
+        //     sum+=i;
+        // }
+        // System.out.println(sum);
+
+        // //5-3
+        // int num = (int)(Math.random()*50)+1;
+        // System.out.println(num);
+
+        // //6
+        // int a=10,b=20;
+        // char oper;
+
+        // Scanner scan = new Scanner(System.in);
+        // oper = scan.next().charAt(0);
+
+        // switch(oper){
+        //     case '+':
+        //         System.out.println(a+b);
+        //         break;
+        //     case '-':
+        //         System.out.println(a-b);
+        //         break;
+        //     case '*':
+        //         System.out.println(a*b);
+        //         break;
+        //     case '/':
+        //         System.out.println(a/b);
+        //         break;
+        // }
+
+        // Scanner scan = new Scanner(System.in);
+        // long a,b,c;
+        // a = scan.nextLong();
+        // b = scan.nextLong();
+        // c = scan.nextLong();
+        
+        // System.out.println(a+b+c);
+        
+        // Scanner scan = new Scanner(System.in);
+
+        // int age = scan.nextInt();
+        // if(age>=20&&age<=29){
+        //     System.out.println("20대");
+        // }else{
+        //     System.out.println("20대 아님");
+        // }
+    }
+}
