a6795df62020a78a92aa77da1a46b7cd6882c7de
ayun0614 <conpaku82@gmail.com>
2023-07-25 19:21:49 +0900

    07-25 실습
    메모리 영역과 배열 참조 타입의 null, NullPointerException 등을 학습했다.

---
+++ 0725 Project.java
@@ -0,0 +1,99 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {    
+        //int[] d = new int[9];
+        
+        //String[] c = {"java", "c", "c++"};
+        
+        //int[] b = {0,1,2,3,4};
+        
+        // char[] a = new char[10];
+        
+
+        
+        // int i = 0, sum = 0;
+        // do{
+        //     i++;
+        //     if(i%2==1)continue;
+        //     sum+=i;
+        // }while(i<10);
+        // System.out.println(sum);
+        
+        
+        // int i=0, sum=0;
+
+        // while(true){
+        //     i=i+2;
+        //     sum+=i;
+        //     if(i>=10)break;
+        // }
+        // System.out.println(sum);
+        
+        
+        
+        // int sum = 0, count = 0;
+        // Scanner scan = new Scanner(System.in);
+        // while(true)
+        // {
+        //     int num = scan.nextInt();
+        //     if(num==0)break;
+
+        //     count++;
+        //     sum+=num;
+        // }
+        // System.out.println((double)sum/count);
+
+
+
+        // int sum = 0;
+        // for(int i=1; i<=10; i++){
+        //     sum+=i;
+        // }
+        // System.out.println(sum);
+
+        // int i = 0, sum = 0;
+
+        // while(i<10){
+        //     i++;
+        //     sum+=i;
+        // }
+        // System.out.println(sum);
+
+        // int i = 0, sum = 0;
+        // do{
+        //     i++;
+        //     sum+=i;
+        // }while(i<10);
+        // System.out.println(sum);
+
+
+
+
+        // int[] a = new int[5];
+        // double[] b = new double[10];
+        // int[] c = new int[3];
+        // char[] d = new char[4];
+
+
+
+        // Scanner scan = new Scanner(System.in);
+        // int count = 0;
+
+        // while(true){
+        //     if(scan.nextInt()==-1) break;
+        //     count++;
+        // }
+        // System.out.println(count);
+        
+
+
+        // for(int i=1;i<=100;i++){
+        //     if(i%2==1){
+        //         continue;
+        //     }else{
+        //         System.out.println(i);
+        //     }
+        // }
+    }
+}
