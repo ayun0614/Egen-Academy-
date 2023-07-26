2e41c605be50b13cb09d0108a1a96602994fb7b1
ayun0614 <conpaku82@gmail.com>
2023-07-26 19:10:43 +0900

    07-26 실습
    for-each, 다차원 배열에 대해서 학습하였습니다.

---
+++ 0726 Project.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {    
+        // double sum = 0.0;
+        // double[] ary = {1.0, 1.5, 2.0, 2.5};
+
+        // for(double i : ary){
+        //     sum+=i;
+        // }
+        // System.out.println(sum);
+        
+        // double sum = 0.0;
+        // double[] ary = {1.0, 1.5, 2.0, 2.5};
+
+        // int i = 0;
+        // do{
+        //     sum+=ary[i];
+        //     i++;
+        // }while(i<ary.length);
+        // System.out.println(sum);
+
+        // double sum = 0.0;
+        // double[] ary = {1.0, 1.5, 2.0, 2.5};
+
+        // int i = 0;
+        // while(i < ary.length)
+        // {
+        //     sum+=ary[i];
+        //     i++;
+        // }
+        // System.out.println(sum);
+        
+        // Scanner scan = new Scanner(System.in);
+
+        // int[] num = new int[10];
+        
+        // for(int i = 0; i < num.length; i++){
+        //     num[i] = scan.nextInt();
+        // }
+
+        // for(int i : num){
+        //     if(i%7==0)System.out.println(i);
+        // }
+
+        
+        // Scanner scan = new Scanner(System.in);
+
+        // int[] num = new int[10];
+        // int sum = 0;
+
+        // for(int i = 0; i < num.length; i++){
+        //     num[i] = scan.nextInt();
+        //     sum+=num[i];
+        // }
+        // System.out.println((double)sum/num.length);
+        
+
+        // String c[][] = {{"java"},{"c","c++"},{"html","css","python"}};
+
+        // for(int i = 0; i < c.length; i++)
+        // {
+        //     for(int j = 0; j < c[i].length; j++)
+        //     {
+        //         System.out.println(c[i][j]);
+        //     }
+        // }
+
+        // char[][] a = {{'T','e','s','t'},{'C','h','e','c','k'}};
+        // double[][] b = new double[5][2];
+        // int[][] c = {{1,2,3},{4,5,6}};
+
+        // for(int[] i : c){
+        //     for(int j : i)
+        //     {
+        //         System.out.print(j+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int i = 0; i < a.length; i++){
+        //     for(int j = 0; j < a[i].length; j++){
+        //         System.out.print(a[i][j]);
+        //     }
+        //     System.out.println();
+        // }
+    }
+}
