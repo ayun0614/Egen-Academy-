b2b3425579f357d2e86023ff02b3ac4dbccfc87c
ayun0614 <conpaku82@gmail.com>
2023-07-28 19:09:28 +0900

    07-28 실습
    이전까지에 범위 복습, 입출력 스트림 등을 학습하였습니다.

---
+++ 0728 Project.java
@@ -0,0 +1,124 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    // static int adder(int a, int b){
+    //     return a+b;
+    // }
+
+    // static double exp(double a){
+    //     return Math.pow(a,2);
+    // }
+
+    // static double returnCircleArea(double r){
+    //     return r*r*3.14;
+    // }
+
+    // static void printCircleArea(double r){
+    //     System.out.println(r*r*3.14);
+    // }
+
+    // static void pr(int[] score, int count){
+    //     for(int i = 0; i < score.length; i++){
+    //         for(int j = 0; j < score[i]/count; j++){
+    //             System.out.print('#');
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+    // static String show(char ch, int num){
+    //     StringBuilder sb = new StringBuilder();
+
+    //     for(int i = 0; i < num; i++){
+    //         sb.append(ch);
+    //     }
+    //     return sb.toString();
+    // }
+
+    public static void main(String[] args) throws Exception { 
+        // String str = "공부는 어렵지만, 재밌네요";
+        // String[] strArr = str.split(",");
+        // System.out.println(strArr[0].charAt(6));
+        // System.out.println(str.substring(str.indexOf("공부는"), str.indexOf("공부는")+3));
+
+        // System.out.println(show('$', 10));
+        
+        // int score[] = new int[]{58,60,86,90,84};
+        // pr(score, 5);
+        
+        // int a = 50, b = 150;
+        // System.out.println(a>b?a*b:a%b);
+        
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+        // StringBuilder sb = new StringBuilder();
+        // int[] numArr = new int[10];
+
+        // for(int i = 0; i < numArr.length; i++){
+        //     numArr[i] = Integer.parseInt(st.nextToken());
+        //     if(numArr[i]%4==0)sb.append(numArr[i]+" ");
+        // }
+
+        // bw.write(sb.toString());
+        // bw.flush();
+        // bw.close();
+        // br.close();
+        
+        // FileReader fr = new FileReader("Test.txt");
+        // BufferedReader br = new BufferedReader(fr);
+
+        // String str;
+
+        // while(true){
+        //     str = br.readLine();
+        //     if(str==null) break;
+
+        //     System.out.println(str);
+        // }
+        
+        // FileWriter fw = new FileWriter("Test.txt");
+        // BufferedWriter bw = new BufferedWriter(fw);
+
+        // String str = "Test입니다.";
+
+        // bw.write("첫번째 줄 입니다.");
+        // bw.newLine();
+        // bw.write(str);
+        // bw.close();
+        // fw.close();
+        
+        // FileWriter fw = new FileWriter("Test.txt");
+        // FileReader fr = new FileReader("Test.txt");
+
+
+        // fw.append("Test");
+        // fw.flush();
+        // fw.close();
+
+        // while(true){
+        //     int data = fr.read();
+        //     if(data==-1)break;
+        //     System.out.print((char)data);
+        // }
+    
+
+        // int[][] intArr = new int[2][5];
+        // int num = 1;
+        // for(int i = 0; i < intArr.length; i++){
+        //     for(int j = 0; j < intArr[i].length; j++){
+        //         intArr[i][j] = num;
+        //         num+=2;
+        //     }
+        // }
+
+        // String a = "C++", b = "JAVA";
+        // System.out.println(a.length());
+        // String c = a.concat(b);
+        // System.out.println(b.substring(3));
+        // b.replace("JAVA", "C#");
+        // String example = "Test,Input";
+        // String[] splitArr = example.split(",");
+    }
+}
